0d22e314967e22ba71e742ee6ab66983a5b959a8
nickfff-dev <hunchographics@gmail.com>
2023-03-17 16:49:29 -0700

    correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits in ascending order
+ * main - entry point
  * Return: ALways 0 (Success)
  */
 int main(void)
