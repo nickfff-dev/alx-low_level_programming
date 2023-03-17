aaef77cbee5d82b12281b7cd400a3adffba01f70
nickfff-dev <hunchographics@gmail.com>
2023-03-17 16:41:26 -0700

    three digit possible combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits in ascending order 
+ * main - prints all possible unique combinations of three digits  
  * Return: Always 0 (Success)
  */
 int main(void)
