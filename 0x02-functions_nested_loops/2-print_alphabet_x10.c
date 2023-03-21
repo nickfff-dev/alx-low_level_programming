b91953dba328e000020a8fc1fd5b94c75473e076
nickfff-dev <hunchographics@gmail.com>
2023-03-21 14:07:05 -0700

    iterate 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,24 +3,22 @@
  * main -entry point of file
  * Return: Always 0
  */
-
 void print_alphabet_x10(void)
 {
-	int count;
 	char dn;
+	int count;
 
-	count =0;
-	while(count < 10)
-	
+	count = 0;
+
+	while (count < 10)
 	{
-	 dn = 'a';
-	 while(dn <= 'z')
-	 {
-		 _putchar(dn);
-		 dn++;
-	 }
-	 _putchar('\n');
-	 count ++;
+		dn = 'a';
+		while (dn <= 'z')
+		{
+			_putchar(dn);
+			dn++;
+		}
+		_putchar('\n');
+		count++;
 	}
-
 }
