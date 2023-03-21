59cdde339a81a71b83058956022b39c6e0e92d81
nickfff-dev <hunchographics@gmail.com>
2023-03-21 13:03:35 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point for main func
+ * Return: int 0 if success
+ */
+void print_alphabet(void)
+{
+ char letter = 'a';
+ while(letter <= 'z')
+ {
+   _putchar(letter);
+   letter++;
+ }; 
+};
+  
+int main(void)
+{
+	print_alphabet();
+	_putchar("/n");
+	return(0);
+}
