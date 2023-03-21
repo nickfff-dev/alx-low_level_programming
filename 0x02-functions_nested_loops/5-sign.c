42e615a2ea49544d8d16bfebebcf7c06b42f6b97
nickfff-dev <hunchographics@gmail.com>
2023-03-21 15:17:04 -0700

    check if less than 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the variable to check the sign
+ * Return: int could be 1 if number is postive 0 if the number is 0 and -1 if the number is negative
+ */
+int print_sign(int n){
+
+	if (n==0)
+	{
+		_putchar('0');
+		return(n);
+	} else if ( n > 0 )
+	{
+		_putchar('+');
+		return(1)
+
+	}
+	_putchar('-');
+	return(-1);
+}
