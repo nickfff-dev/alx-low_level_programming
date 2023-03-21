d727fe4004ecfc0f6c21b690116e47d271151315
nickfff-dev <hunchographics@gmail.com>
2023-03-21 14:18:03 -0700

    check case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * Return: 1 if c is lowercase, 0 if uppercase
+ */
+int _islower(int c)
+{
+	if (ch >= 'A' and ch <= 'Z')
+	{
+		return 0
+	} else
+	{
+	
+		return 1
+	}
+}
