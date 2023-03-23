f92413231be094449d637cc21988884f15b35c86
nickfff-dev <hunchographics@gmail.com>
2023-03-23 07:57:14 -0700

    chec if is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - takes a parameter andd checks if it is a digit
+ * @c: the variable to be cheecked
+ * Return: always 0
+ */
+
+int _isdigit(int c)
+{
+
+	return (c >= '0' && c <= '9');
+}
