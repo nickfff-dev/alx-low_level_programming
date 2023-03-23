2b3f533e0c3f406e339bd96b22aa1b64bfa36ab7
nickfff-dev <hunchographics@gmail.com>
2023-03-23 07:47:00 -0700

    more fucntions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stlib.h>
+#include <stdio.h>
+/**
+ * _isupper - checks case of a character
+ * @c: the varibale to be checked
+ * Return: always 0 if success
+ */
+int _isupper(int c)
+{
+
+	return (c >= 'A' && c <= 'Z');
+}
