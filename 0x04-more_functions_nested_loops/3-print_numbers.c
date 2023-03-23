58f4d77002d4f5a318532e9f8ed90ffbf86000e8
nickfff-dev <hunchographics@gmail.com>
2023-03-23 12:08:28 -0700

    print-numbrs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,15 @@
  * print_numbers - prints digits 0-9
  */
 void print_numbers(void)
+	char c;
+	for (i ='0'; i <= '9'; i++)
 {
 
-	char i;
-	for (i= '0'; i <= '9'; i++)
+	if (i != '2' && i != '4')
 	{
-		if( i != '2' && i !='4')
-			_putchar(i);
-		
+	
+		_putchar (i);
 	}
+}
 	_putchar('\n');
 }
