c46ef3a6244c96993b71b4106de6f2d24067266f
nickfff-dev <hunchographics@gmail.com>
2023-03-23 11:26:52 -0700

    multipy ints

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two intergers
+ * @a: the first int to be multiplied
+ * @b: the second int to be multiplied
+ * Return: the value of the product
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
