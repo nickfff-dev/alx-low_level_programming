3ece94370985562c46e9e48e51488e1d26d1fa39
nickfff-dev <hunchographics@gmail.com>
2023-04-03 16:59:32 -0700

    check for c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include  "main.h"
+/**
+ * _strchr - check for char in string
+ * @s: the string to search
+ * @c: the char to find
+ * Return: a pointer to the char found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (true)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		} if (*(s + i) == 0)
+		{
+			return (NULL);
+		}
+		i++;
+	}
+}
