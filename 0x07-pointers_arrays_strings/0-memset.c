02546e4aa2539c42bcfea34f6665293e7b3d5800
nickfff-dev <hunchographics@gmail.com>
2023-04-03 16:09:14 -0700

    print buffer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,12 @@
  * Return: a pointer to the memory area
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+		*(s + i) = b;
 	}
 	return (s);
 }
