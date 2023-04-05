ad87978552f2374f69450e5e2c4544aaf5ddef35
nickfff-dev <hunchographics@gmail.com>
2023-04-05 16:50:19 -0700

    print in recerse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * void _print_rev_recursion - print string on reverse
+ * _print_rev_recursion - print string on reverse
  * @s: string to be printed
  * Return: void
  */
@@ -9,7 +9,6 @@ void _print_rev_recursion(char *s)
 
 	if (*(s) != 0)
 	{
-	
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
