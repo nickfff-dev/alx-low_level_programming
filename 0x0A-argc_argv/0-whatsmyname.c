9808e1fc2c3d74064350a47db4d6111a106a2ffc
nickfff-dev <hunchographics@gmail.com>
2023-04-11 11:21:20 -0700

    comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - prints first argumnent of main
+ * @argc: the number of variables passed
+ * @argv: the array of cli variables
+ * Return: exit success
+ */
+
+
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
