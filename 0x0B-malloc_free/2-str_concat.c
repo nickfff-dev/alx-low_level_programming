2b49f6265c5a0e130f188790188dc1008d1f4db1
nickfff-dev <hunchographics@gmail.com>
2023-04-12 16:06:02 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,37 +9,40 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int reslen;
-	char *res;
-	int len1 = 0;
-	int len2 = 0;
-	int i = 0;
-	int count = 0;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
-	len1++;
+		len1++;
 	while (s2 && s2[len2])
-	len2++;
-	reslen = len1 + len2;
-	if (reslen == 0)
-	{
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	}
-	res = (char *)malloc((reslen + 1) * sizeof(char));
-	if (res == NULL)
+
+	i = 0;
+	j = 0;
+
+	if (s1)
 	{
-		return (NULL);
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
-	while (i < len1)
+
+	if (s2)
 	{
-		*(res + i) = s1[i];
-		i++;
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
-	while (count < len2)
-	{
-		*(res + i + count) = s2[count];
-		count++;
-	}
-	res[reslen] = '\0';
-	return (res);
+	s3[i] = '\0';
+
+	return (s3);
 }
