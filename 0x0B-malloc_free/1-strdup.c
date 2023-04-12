6915db9ca7bd451a0d6317b09fb54ad3f46939dd
nickfff-dev <hunchographics@gmail.com>
2023-04-12 15:34:09 -0700

    concat string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,35 +2,43 @@
 #include <stdlib.h>
 
 /**
- * _strdup - returns pointer to new array
- * @str: the string to copy
- * Return: pointer or null
- */
-
+* _strdup - returns pointer to new array
+* @str: the string to copy
+* Return: pointer or null
+*/
 char *_strdup(char *str)
 {
-	char *y;
-	int num;
+	int reslen;
+	char *res;
+	int len1 = 0;
+	int len2 = 0;
+	int i = 0;
+	int count = 0;
 
-	if (str == NULL)
+	while (s1[len1])
+	len1++;
+	while (s2[len2])
+	len2++;
+	reslen = len1 + len2;
+	if (reslen == 0)
 	{
 		return (NULL);
 	}
-	num = 0;
-	while (str[num])
-	{
-		num++;
-	}
-	y = (char *)malloc((num + 1) * sizeof(char));
-
-	if (y == NULL)
+	res = (char *)malloc((reslen + 1) * sizeof(char));
+	if (res == NULL)
 	{
 		return (NULL);
 	}
-
-	while (num--)
+	while (i < len1)
 	{
-		*(y + num) = str[num];
+		*(res + i) = s1[i];
+		i++;
 	}
-	return (y);
+	while (count < len2)
+	{
+		*(res + i + count) = s2[count];
+		count++;
+	}
+	res[reslen] = '\0';
+	return(res);
 }
