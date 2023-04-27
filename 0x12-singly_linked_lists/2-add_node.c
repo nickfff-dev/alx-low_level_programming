b6667657d7eb80c5bfbeb6d54c46b653eb6e8237
nickfff-dev <hunchographics@gmail.com>
2023-04-27 22:13:09 +0000

    add a node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,13 +8,16 @@
 * @str: a pointer to a string
 * Return: the address of the new element, or NULL if it failed
 */
+
 list_t *add_node(list_t **head, const char *str)
 {
 		list_t *l;
 		unsigned int count = 0;
 
 		while (str[count])
-		count++;
+		{
+			count++;
+		}
 		l = malloc(sizeof(list_t));
 		if (!l)
 		{
