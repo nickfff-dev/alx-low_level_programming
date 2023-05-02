6b0a2b402803f8831a6a5f565e4596bb5e40c73e
nickfff-dev <hunchographics@gmail.com>
2023-05-02 20:47:58 +0000

    error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	listint_t *ptr = h;
 	size_t count = 0;
 
-	while (ptr != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", ptr->n);
-		ptr = ptr->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 	return (count);
