505376c9afd9121d64fafc97309a8661d986c2e2
nickfff-dev <hunchographics@gmail.com>
2023-05-02 20:58:20 +0000

    count of nodes

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 /**
  * listint_len - function that count nodes in a sl
- * @h -  head of the sl
+ * @h: head of the sl
  * Return: count of the nodes
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
-	while(h)
+
+	while (h)
 	{
 		count++;
 		h = h->next;
