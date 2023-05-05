04a968c6b4988fdba3bc2620ccc0ebaf7c98a9d0
nickfff-dev <hunchographics@gmail.com>
2023-05-05 16:45:56 +0000

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * print_binary - the function name
+  * @n: parameter of type unsigned long int .
+  * Return: void .
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	for (i = 63; i >= 0; i--)
+	{
+		k = n >> i;
+		if (k & 1)
+		{
+			j = 1;
+			printf("1");
+		}
+		else if (j == 1)
+		{
+			printf("0");
+		}
+	}
+}
