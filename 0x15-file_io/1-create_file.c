3ee6d237ce6d840433314e26dfe02ad811b79d7a
nickfff-dev <hunchographics@gmail.com>
2023-07-11 01:31:19 +0300

    try

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	for (i = 0; text_content[i] != '\0'; i++)
+	{ 
 		;
+	}
 	w = write(fd, text_content, i);
+	if (w == -1)
+	{
+		return (-1);
+	}
 	close(fd);
 	return (1);
 }
