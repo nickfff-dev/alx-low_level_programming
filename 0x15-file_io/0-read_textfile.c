07a7f78c3fd90a9a8a74b06b01caece70012161f
nickfff-dev <hunchographics@gmail.com>
2023-07-11 00:48:45 +0300

    read write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+* read_textfile - function reads text file prints to stdin
+* @filename: pointer to the file name
+* @letters: number of characters to read
+* Return: number of chars read and written or 0
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t s, w;
+	char *c;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		return (0);
+	}
+	c = (char *)malloc(sizeof(char) * letters);
+	if (c == NULL)
+	{
+		return (0);
+	}
+	s = read(fd, c, letters);
+	if (s <= 0)
+	{
+		return (0);
+	}
+	w = write(STDOUT_FILENO, c, s);
+
+	if (w <= 0)
+	{
+		return (0);
+	}
+
+	if (w != s)
+	{
+		return (0);
+	}
+
+	close(fd);
+	free(c);
+	return (s);
+}
