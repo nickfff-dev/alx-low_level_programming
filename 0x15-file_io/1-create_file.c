8f44a32ea5f048083f57819ac93f06a706ee2150
nickfff-dev <hunchographics@gmail.com>
2023-07-11 01:23:34 +0300

    betty error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,29 @@
 #include "main.h"
 
 /**
-  * create-file - function that creates a file
-  * @filename: pointer to the file
-  * @text_content: content to write to file
-  * Return: 1 0r -1
-  */
-
+  * create_file - the function name
+  * @filename: parameter of type const char *.
+  * @text_content: parameter of type char *.
+  * Return: int .
+ */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, i, w;
+	int fd, i, w;
 
-    if (filename == NULL)
-        return (-1);
-
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
-
-    if (text_content == NULL)
-        return (1);
-
-    for (i = 0; text_content[i] != '\0'; i++)
-        ;
-
-    w = write(fd, text_content, i);
-    if (w == -1)
-        return (-1);
-
-    close(fd);
-    return (1);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+		;
+	}
+	w = write(fd, text_content, i);
+	close(fd);
+	return (1);
 }
