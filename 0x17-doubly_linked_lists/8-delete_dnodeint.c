240f83ed0f750b7ca237881fd5a9ff4deca76f82
nickfff-dev <hunchographics@gmail.com>
2023-08-19 02:22:29 +0300

    corrections

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -34,6 +34,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 		current = current->next;
 	}
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	if (current->prev != NULL)
+		current->prev->next = current->next;
 	free(current);
 	return (1);
 }
