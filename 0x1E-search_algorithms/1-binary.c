4812987533a31e76c9be77e3df516067832daa1a
nickfff-dev <hunchographics@gmail.com>
2023-12-14 00:38:01 +0300

    binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,30 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* print_array - Prints an array of integers
+* @array: The array to be printed
+* @start: Index to start printing from
+* @end: Index to stop printing
+*/
+
+void print_array(int *array, int start, int end)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = start; i < end; i++)
+	{
+		printf("%d", array[i]);
+		if (i < end - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+
 /**
  * binary_search -  function that uses the binary search algo
  * @array: pointer to beginning of the list
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,4 @@
 
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
-void print_array(int *array, int start, int end);
 #endif
